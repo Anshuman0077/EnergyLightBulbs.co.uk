4220784bf202c191f8057ac34939d4473762b5fe
itz-lucifer001 <itzluciferaloneboy@gmail.com>
2025-06-14 18:20:34 +0530

    add basic category section and modify folder and file structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { Inter, Roboto, Oswald } from "next/font/google";
 import "./globals.css";
 // import Header from "./components/header";
 
-import Header from "./components/header";
-import { Footer } from "./components/footer";
-import { BottomFoot } from "./components/bottomfoot";
+import Header from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { BottomFoot } from "./components/footer/bottomfoot";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -44,41 +44,33 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${roboto.variable} ${oswald.variable} antialiased`}
       >
-          <div className="flex flex-col min-h-screen max-w-[1800px]  text-text1">
-            <div className="px-16">
-              <Header />
 
+        <div className="flex flex-col min-h-screen max-w-[1800px]  text-text1">
+          <div className="px-18">
+            <Header />
+          </div>
+
+          <main className="flex-1 ">{children}</main>
+
+          <div className="w-full  bg-bg5 mt-3 border-t-3 border-border6 px-18 ">
+            <Footer />
+          </div>
+
+          <div className="py-3">
+            <BottomFoot />
+          </div>
+
+          <div className="w-full px-6 bg-bg5 sm:px-12 lg:px-18 py-6 text-center text-sm text-text7  border-border5">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              <p>Copyright © 2025 Energy Light Bulbs Ltd.</p>
+              <img
+                src="https://doo9vxlv0gkqf.cloudfront.net/media/checkout_payment.png"
+                alt="Payment Options"
+                className="mt-4 md:mt-0 w-72"
+              />
             </div>
-      {/* Header */}
-      
-
-      {/* Main Content */}
-      <main className="flex-1 ">{children}</main>
-
-      {/* Footer Top Section (you can use Footer.jsx or write inline here) */}
-      <div className="w-full  bg-bg5 mt-3 border-t-3 border-border6 ">
-        <Footer />
-      </div>
-
-      {/* Bottom Footer Full Width */}
-      <div className="py-3">
-        <BottomFoot />
-
-      </div>
-      
-
-      {/* Copyright */}
-      <div className="w-full px-6 bg-bg5 sm:px-12 lg:px-18 py-6 text-center text-sm text-text7  border-border5">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <p>Copyright © 2025 Energy Light Bulbs Ltd.</p>
-          <img
-            src="https://doo9vxlv0gkqf.cloudfront.net/media/checkout_payment.png"
-            alt="Payment Options"
-            className="mt-4 md:mt-0 w-72"
-          />
+          </div>
         </div>
-      </div>
-    </div>
       </body>
     </html>
   );
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-      <div>
-        {/* <h1 className="font-bold text-2xl text-green-400">energyLightBulbs.co.uk</h1> */}
-      </div>
-  );
-}
