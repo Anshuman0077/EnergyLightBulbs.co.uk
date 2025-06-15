feb0a3b8ec7bbbc60e71431c8dafc9b255d04689
itz-lucifer001 <itzluciferaloneboy@gmail.com>
2025-06-16 04:26:44 +0530

    implement the category section

--- app/components/category/categoryData.tsx
+++ app/components/category/categoryData.tsx
@@ -1,31 +1,50 @@
-import React from 'react'
+import React from "react";
+import { SubCategory } from "./categoryList";
 
-export const CategoryData = () => {
-  return (
-   <div>
-    <div className='w-full text-black px-10 font-bold'>
-       <ul className='w-full flex space-x-10 '>
-        <li>Halogen Bulbs</li>
-        <li>Energy Saving Compact Fluorescent</li>
-        <li>Low Voltage Bulbs</li>
-        <li>Incendescent</li>
-        <li>Sodium</li>
-        <li>Decordation LED Bulbs</li>
-        <li>LED Panel</li>
-        <li>Fire Rated Down Lights</li>
-        <li>Celing Down Lights</li>
-        <li>Cabinet/UnderSelf Lighting</li>
-        <li>Transforms / LED Drivers</li>
-       </ul>
-    </div>
-    <div>
-        <ul className='text-black'>
-            <li>
-                <div>Img</div>
-                <div>Name</div>
-            </li>
-        </ul>
-    </div>
-   </div>
-  )
+interface Props {
+  subcategories: SubCategory[];
+  mainCategoryName: string;
 }
+
+export const CategoryData: React.FC<Props> = ({ subcategories, mainCategoryName }) => {
+  return (
+    <div className="w-full grid grid-cols-5 auto-rows-auto gap-x-3 px-4 py-2 text-black">
+      {subcategories.map((sub, index) => {
+        const isFirstMainCategory = mainCategoryName === "Light Bulbs";
+
+        const positionClass = isFirstMainCategory
+          ? index === subcategories.length - 3
+            ? "col-start-3 row-start-2"
+            : index === subcategories.length - 2
+            ? "col-start-4 row-start-2"
+            : index === subcategories.length - 1
+            ? "col-start-5 row-start-2"
+            : ""
+          : "";
+
+        return (
+          <div key={index} className={`flex flex-col mb-4 ${positionClass}`}>
+            <h3 className="font-semibold text-md border-b py-2 mb-2">{sub.name}</h3>
+            <div className="flex flex-col gap-3">
+              {sub.items.map((item, idx) => (
+                <div
+                  key={idx}
+                  className="flex items-center gap-3 transition-all duration-500 ease-in-out hover:translate-x-2 text-text13 hover:text-text20 hover:opacity-80"
+                >
+                  <img
+                    src={item.img}
+                    alt={item.imgName}
+                    className="w-14 h-14 border p-1 border-border7 object-contain rounded transition-transform duration-500 ease-in-out hover:scale-105"
+                  />
+                  <span className="text-xs transition-all duration-500 ease-in-out">
+                    {item.imgName}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
